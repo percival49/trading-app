56e06f82ebe877ff7b756186d45027287de35691
Kate Pelinggon <katepelinggon@gmail.com>
2022-12-17 11:28:02 +0800

    create view all users, view user, and edit user

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root "posts#index"
   devise_for :users
   resources :posts
+
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
